b6808b972277ef304d2faa8d4caf2cd851215a81
neko_0xff <54382007+neko0xff@users.noreply.github.com>
2023-12-25 05:09:21 +0800

    20231225 update

--- ansible/firewall.sh
+++ ansible/firewall.sh
@@ -1,5 +1,5 @@
 inventory=cnServers
-playbook1=host_firewall.sh
-playbook2=contalner_firewall.sh
+playbook1=host_firewall.yml
+playbook2=container_firewall.yml
 ansible-playbook -i $inventory $playbook1
 ansible-playbook -i $inventory $playbook2
